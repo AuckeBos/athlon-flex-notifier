73beae56432305915eac3be927695aba66c2f59d
Aucke Bos <aucke.bos97@gmail.com>
2024-11-11 21:52:19 +0100

    feat: split notify and refresh. listen on event + daily schedule

--- src/athlon_flex_notifier/flows.py
+++ src/athlon_flex_notifier/flows.py
@@ -1,6 +1,7 @@
 from kink import di
-from prefect import flow, serve, task
+from prefect import flow, serve
 from prefect.client.schemas.schedules import CronSchedule
+from prefect.events import DeploymentEventTrigger
 
 from athlon_flex_notifier.notifications.notifiers import Notifiers
 from athlon_flex_notifier.refresher import (
@@ -8,35 +9,45 @@ from athlon_flex_notifier.refresher import (
 )
 
 
-@task
+@flow
 def refresh() -> None:
     """Update the database with the current vehicles."""
     di[Refresher].refresh()
 
 
-@task
+@flow
 def notify() -> None:
     """Notify the user about new vehicles."""
     di[Notifiers].notify()
 
 
-@flow
-def refresh_and_notify() -> None:
-    """Update availabilities and notify the user."""
-    refresh()
-    notify()
-
-
 def work() -> None:
     """Create the deployments."""
     serve(
-        refresh_and_notify.to_deployment(
-            name="refresh_and_notify",
-            version="2024.11.03",
+        refresh.to_deployment(
+            name="refresh",
+            version="2024.11.11",
             schedules=[
                 CronSchedule(
                     cron="*/10 * * * *",
                 )
             ],
-        )
+        ),
+        notify.to_deployment(
+            name="notify",
+            version="2024.11.11",
+            triggers=[
+                DeploymentEventTrigger(
+                    name="notify_on_refresh",
+                    enabled=True,
+                    match_related={"prefect.resource.name": "refresh"},
+                    expect=["prefect.flow-run.Completed"],
+                )
+            ],
+            schedules=[
+                CronSchedule(
+                    cron="0 6 * * *",
+                )
+            ],
+        ),
     )
